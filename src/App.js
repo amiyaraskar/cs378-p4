31066ff92f6c49ca1cb40e370f02881da07a495e
Amiya <amiyaraskar@gmail.com>
2024-03-03 15:09:08 -0600

    added src folder

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { useState } from 'react';
+// This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+
+function App() {
+  
+}
+
+export default App;
